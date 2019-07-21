98bf8b4c32660ca7bf975fc6aa5532dbba6a5546
James Cape <james.cape@gmail.com>
2019-07-21 16:24:58 -0600

    Changes "commit messages" to "commit log" in message params

--- app/models/message.rb
+++ app/models/message.rb
@@ -4,4 +4,10 @@ class Message < ApplicationRecord
                         :commit_log,
                         :creator
 
+  def initialize(attributes)
+    @build_status = attributes[:build_status]
+    @build_state = attributes[:build_state]
+    @commit_log = attributes[:commit_log]
+    @creator = attributes[:creator]
+  end
 end
