53a13c32768aa86ead04ab84f02b2604fbbbd9cc
James Cape <james.cape@gmail.com>
2019-07-21 15:57:04 -0600

    Adds line

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,10 +1,11 @@
 class WebhooksController < ApplicationController
   def index
-    # require 'pry'; binding.pry
+    require 'pry'; binding.pry
     message = Message.new(message_params)
     message.save
 
   end
+
 
   def message_params
     require(:message).permit(:build_status, :build_state, :commit_messages, :creator)
