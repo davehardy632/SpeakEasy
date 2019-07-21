c39b8bb9461d8b48a1a173c37f21d03b3e704beb
James Cape <james.cape@gmail.com>
2019-07-21 15:29:03 -0600

    Adds message model and creates a message in the controller

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,7 @@
 class Message < ApplicationRecord
+  validates_presence_of :build_status,
+                        :build_state,
+                        :commit_log,
+                        :creator
+
 end
