1abb38c89bd83e8188b91215b64f3dc1dd77f87b
David Hardy <davehardy632@gmail.com>
2019-07-21 23:20:25 -0600

    built endpoints for consumption by speakeasy-sinatra microservice

--- spec/requests/api/v1/messages_request_spec.rb
+++ spec/requests/api/v1/messages_request_spec.rb
@@ -2,15 +2,18 @@ require 'rails_helper'
 
 describe 'Messages Api' do
   it "sends a list of messages" do
-    message_1 = create(:message)
-    message_2 = create(:message)
-    message_3 = create(:message)
+    message_1 = create(:message, commit_messages: "hello there is a new commit waiting", creator: "John")
+    message_2 = create(:message, commit_messages: "hello there is a new commit waiting again", creator: "Ted")
+    message_3 = create(:message, commit_messages: "hello there is a new commit waiting for a third time", creator: "Lauren")
 
     get '/api/v1/messages.json'
 
     expect(response).to be_successful
 
     messages = JSON.parse(response.body)
-    expect(customers["data"].count).to eq(3)
+
+    expect(messages["data"].count).to eq(3)
+    expect(messages["data"][0]["attributes"]["creator"]).to eq("John")
+    expect(messages["data"][0]["attributes"]["commit_messages"]).to eq("hello there is a new commit waiting")
   end
 end
