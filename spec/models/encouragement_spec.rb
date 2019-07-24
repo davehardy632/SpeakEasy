a56710925e7204462f9c15918bef2ac1f98ee3c4
James Cape <james.cape@gmail.com>
2019-07-24 11:01:57 -0600

    Adds message and encouragement model tests

--- spec/models/encouragement_spec.rb
+++ spec/models/encouragement_spec.rb
@@ -1,5 +1,27 @@
-require 'rails_helper'
+require "rails_helper"
 
-RSpec.describe Encouragement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+RSpec.describe Encouragement do
+  it "has attributes" do
+    attributes =  {
+      creator: "Martin",
+      motivation: "You did great!"
+    }
+
+    encouragement = Encouragement.new(attributes)
+
+    expect(encouragement.creator).to eq(attributes[:creator])
+    expect(encouragement.motivation).to eq(attributes[:motivation])
+  end
+
+  it "has different attributes" do
+    attributes =  {
+      creator: "Sam",
+      motivation: "You did amazing!"
+    }
+
+    encouragement = Encouragement.new(attributes)
+
+    expect(encouragement.creator).to eq(attributes[:creator])
+    expect(encouragement.motivation).to eq(attributes[:motivation])
+  end
 end
